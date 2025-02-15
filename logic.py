16c1f75784aaac29d4cbac4153d8bfbae1eef5d7
Nikita-2900 <neckit2900@gmail.com>
2025-02-15 14:27:41 +0500

    pokemon2

--- logic.py
+++ logic.py
@@ -7,15 +7,12 @@ class Pokemon:
     def __init__(self, pokemon_trainer):
 
         self.pokemon_trainer = pokemon_trainer   
-
         self.pokemon_number = randint(1,1000)
+        self.hp = randint(50, 200)
+        self.power = randint(5, 50)
         self.img = self.get_img()
         self.name = self.get_name()
-
         Pokemon.pokemons[pokemon_trainer] = self
-    
-
-
     # Метод для получения картинки покемона через API
     def get_img(self):
         url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'
@@ -39,12 +36,46 @@ class Pokemon:
 
     # Метод класса для получения информации
     def info(self):
-        return f"Имя твоего покеомона: {self.name}"
+        return f"""Имя твоего покеомона: {self.name}
+        здоровье твоего покемона: {self.hp}
+        сила твоего покемона: {self.power}"""
 
     # Метод класса для получения картинки покемона
     def show_img(self):
         return self.img
     
+    def attack(self, enemy):
+        if isinstance(enemy, Wizard):
+            chance = randint(1,5)
+            if chance == 1:
+                return "Покемон-волшебник применил щит в сражении"
+        if enemy.hp > self.power:
+            enemy.hp -= self.power
+            return f"""Сражение @{self.pokemon_trainer} с @{enemy.pokemon_trainer}
+            Здоровье @{enemy.pokemon_trainer} теперь {enemy.hp}"""
+        else:
+            enemy.hp = 0
+            return f"Победа @{self.pokemon_trainer} над @{enemy.pokemon_trainer}! "
+    
+    def korm(self):
+        lek = randint(1,10)
+        self.hp += lek
+        return f"Вы покормили своего покемона, теперь его здоровье: {self.hp}"
+        
+class Wizard(Pokemon):
+    def info(self):
+        return "У тебя покемон-волшебник \n\n" + super().info()
+
+class Fighter(Pokemon):
+    def attack(self, enemy):
+        super_power = randint(5,15)
+        self.power += super_power
+        result = super().attack(enemy)
+        self.power -= super_power
+        return result + f"\nБоец применил супер-аттаку силой:{super_power}"
+    def info(self):
+        return "У тебя покемон-боец\n\n" + super().info()
+        
 
 
 
